5c6d0ab9d37881c77e3caad7cc78c01d12dd777c
Hosszú Tamás <hosszutamas@ktch.hu>
2024-03-27 11:21:21 +0100

    Add project files.

---
+++ ConsoleApp4/harom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class haromszog
+    {
+        public haromHossz()
+        {
+           
+        }
+
+        static int Hossz(int SideA, int SideB, int SideC)
+        {
+            SideA = int(Console.Write("A oldal hossza: "));
+            SideB = int(Console.Write("B oldal hossza: "));
+            SideC = int(Console.Write("C oldal hossza: "));
+        }
+
+        static void kerulet()
+        {
+            return
+        }
+
+        static void terulet()
+        {
+
+        }
+    }
+}
